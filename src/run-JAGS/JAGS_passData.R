1e18ee44c5853909d54f015ae7703e8050600425
Adriana Chavez <adrifelcha@gmail.com>
2025-06-09 19:47:33 -0700

    Neglectable commit. Removed a space

--- src/run-JAGS/JAGS_passData.R
+++ src/run-JAGS/JAGS_passData.R
@@ -6,8 +6,7 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
 JAGS_passData <- function(modelType=NA){
   # Define the basic data variables needed for all model types
-  passData <- list("nParticipants", "nTrialsPerPerson",
-                   "meanRT", "varRT", "correct")
+  passData <- list("nParticipants", "nTrialsPerPerson","meanRT", "varRT","correct")
   
   # For models that include predictors (i.e., metaregression, t-test),
   # we also need to pass the predictor vector X
