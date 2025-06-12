5646c6f75556f7dad7feb866b90a7ec992c7584d
Adriana Chavez <adrifelcha@gmail.com>
2025-06-11 17:32:23 -0700

    Rewritting function - The Rhat calculation goes in /calculators/. This script should provide functions to handle the (already calculated) rhat values

--- src/run-JAGS/JAGS_getRhat.R
+++ src/run-JAGS/JAGS_getRhat.R
@@ -9,46 +9,19 @@
 # - posterior_chains: A matrix where columns store MCMC chains and rows store iterations
 # - n.chains: Number of chains (only needed if posterior_chains has been reshaped into a vector)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
-getRhat <- function(posterior_chains, n.chains=NA) {
-        # Ensure the input is a matrix
-        # If a vector is provided, reshape it into a matrix with n.chains columns
-        if (is.vector(posterior_chains)) {
-            if(is.na(n.chains)){
-                  stop("Please specify the number of chains") 
-            }else{
-              # Calculate iterations per chain and reshape the vector into a matrix
-              n.iter <- length(posterior_chains)/n.chains
-              # We assume that chains had been stacked in the vector
-              posterior_chains <- matrix(posterior_chains, ncol=n.chains, nrow = n.iter, byrow = FALSE)
-            }
+JAGS_Rhat <- function(runJags, withinSubject = FALSE, EZRobust = FALSE, separate_datasets = FALSE) {
+    if(EZRobust){
+        if(withinSubject){
+            rhats <- runJags$BUGSoutput$Rhat[c("bound_mean", "drift_mean", "nondt_mean", "betaweight")]
         }else{
-            # Get dimensions of the posterior chains matrix
-            n.iter <- nrow(posterior_chains)  # Number of iterations per chain
-            n.chains <- ncol(posterior_chains)  # Number of chains
+            rhats <- runJags$BUGSoutput$Rhat[c("bound_mean", "drift_mean", "nondt_mean", "betaweight")]
         }
-        
-        # Step 1: Compute the mean of each chain
-        chainMean <- apply(posterior_chains, 2, mean)
-        
-        # Step 2: Compute the mean of the chain means
-        overall_mean <- mean(chainMean)
-        
-        # Step 3: Compute between-chain variance (B)
-        B <- n.iter * var(chainMean)
-        
-        # Step 4: Compute within-chain variances (W)
-        chainVar <- apply(posterior_chains, 2, var)
-
-        # Step 5: Compute the mean of the within-chain variances
-        W <- mean(chainVar)
-        
-        # Step 6: Estimate the marginal posterior variance 
-        Z <- ((n.iter - 1) / n.iter) * W + (1 / n.iter) * B
-        
-        # Step 7: Compute R-hat
-        Rhat <- sqrt(Z / W)
-  
-  return(Rhat)
+    }
 }
 
 # Reference: https://medium.com/@nialloulton/understanding-the-r-hat-statistic-d83b3b5ca162
+
+runJags$EZ$clean
+runJags$EZ$contaminated
+runJags$EZRobust$clean
+runJags$EZRobust$contaminated
