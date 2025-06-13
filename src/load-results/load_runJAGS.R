a48e42d622739082c9cca5534e23811b0ba53777
Adriana Chavez <adrifelcha@gmail.com>
2025-06-12 19:45:46 -0700

    Add: Custom functions to load the JAGS output generated with the runJAGS function

--- src/load-results/load_runJAGS.R
+++ src/load-results/load_runJAGS.R
@@ -1,27 +1,51 @@
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
-# This function calculates the R-hat convergence diagnostic
-#
-# R-hat measures the convergence of MCMC chains by comparing the variance between
-# chains to the variance within chains. Values close to 1.0 indicate good convergence.
-# Generally, we consider values below 1.05 to be acceptable.
-#
-# Inputs:
-# - posterior_chains: A matrix where columns store MCMC chains and rows store iterations
-# - n.chains: Number of chains (only needed if posterior_chains has been reshaped into a vector)
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
-JAGS_Rhat <- function(runJags, withinSubject = FALSE, EZRobust = FALSE, separate_datasets = FALSE) {
-    if(EZRobust){
-        if(withinSubject){
-            rhats <- runJags$BUGSoutput$Rhat[c("bound_mean", "drift_mean", "nondt_mean", "betaweight")]
-        }else{
-            rhats <- runJags$BUGSoutput$Rhat[c("bound_mean", "drift_mean", "nondt_mean", "betaweight")]
-        }
-    }
+jags_localResults <- function(runJags_i, EZ_variation, data_variation, true_parameter_set, this.seed){
+    localResults <- list(seed = this.seed,           # Seed used for this cell (could change if R-hats were bad)
+                         p = runJags_i$jags_data$nParticipants,                      # Number of participants
+                         t = runJags_i$jags_data$nTrialsPerCondition,                # Number of trials
+                         ez_type = EZ_variation,                                           # Design type                         
+                         data_type = data_variation,
+                         rhats = runJags_i$rhats,      # Convergence diagnostics
+                         true.values = true_parameter_set,      # True parameter values
+                         mean.estimates = runJags_i$estimates,      # Posterior means
+                         std.estimates = runJags_i$estimates,       # Posterior SDs
+                         elapsed.time = runJags_i$clock             # Computation time
+                        )
+    return(localResults)
 }
 
-# Reference: https://medium.com/@nialloulton/understanding-the-r-hat-statistic-d83b3b5ca162
+runJAGS_cellResults <- function(runJags){
+    
+    EZ_variations <- names(runJags)
+    data_variations <- names(runJags$EZ)
+    
+    output <- list()
+    for(EZ_var in EZ_variations){
+        if(length(runJags[[EZ_var]]) > 1){            
+            for(data_var in data_variations){                
+                # Create name for this combination
+                name <- paste0(EZ_var, "_", data_var)
+                # Store results in named list element
+                output[[name]] <- jags_localResults(runJags_i = runJags[[EZ_var]][[data_var]], 
+                                                  EZ_variation = EZ_var, 
+                                                  data_variation = data_var, 
+                                                  this.seed = this.seed,
+                                                  true_parameter_set = parameter_set)
+            }            
+        }else{
+            name <- paste0(EZ_var, "_", runJags[[EZ_var]]$data_type)
+            output[[name]] <- jags_localResults(runJags_i = runJags[[EZ_var]], 
+                                              EZ_variation = EZ_var, 
+                                              data_variation = runJags[[EZ_var]]$data_type,
+                                              this.seed = this.seed, 
+                                              true_parameter_set = parameter_set)
+        }     
+    }
 
-runJags$EZ$clean
-runJags$EZ$contaminated
-runJags$EZRobust$clean
-runJags$EZRobust$contaminated
+    rhats <- c()
+    for(name in names(output)){
+        rhats <- c(rhats, output[[name]]$rhats)
+    }    
+
+    final_output <- list(rhats = rhats, output = output)
+    return(final_output)
+}
