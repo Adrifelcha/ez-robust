2b7e7ab757ba7c17d65ba329671df40d12ecf257
Adriana Chavez <adrifelcha@gmail.com>
2025-06-17 17:56:03 -0700

    Add: The function now takes a data input and uses it to pass a complete list

--- src/run-JAGS/JAGS_passData.R
+++ src/run-JAGS/JAGS_passData.R
@@ -4,27 +4,31 @@
 # Inputs:
 # - modelType: Type of model being used (e.g., "hierarchical", or "ttest")
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
-JAGS_passData <- function(modelType=NA, EZRobust=FALSE, withinSubject=FALSE){
+JAGS_passData <- function(EZ_stats, modelType=NA, EZmodel=FALSE, withinSubject=FALSE){
   # Define the basic data variables needed for all model types
-  passData <- list("nParticipants", "correct")
+  passData <- list("nParticipants" = EZ_stats$nParticipants, 
+                   "correct" = EZ_stats$correct)
 
-  if(EZRobust){
-    passData <- c(passData, "medianRT", "iqrVarRT")
+  if(EZmodel == "EZRobust"){
+    passData <- c(passData, "medianRT" = EZ_stats$medianRT, 
+                            "iqrVarRT" = EZ_stats$iqrVarRT)
   }else{
-    passData <- c(passData, "meanRT", "varRT")
+    passData <- c(passData, "meanRT" = EZ_stats$meanRT, 
+                            "varRT" = EZ_stats$varRT)
   }
 
   if(withinSubject){
-    passData <- c("nTrialsPerCondition", "P", passData)
+    passData <- c("nTrialsPerCondition" = EZ_stats$nTrialsPerCondition, 
+                  "P" = EZ_stats$P, passData)
   }else{
-    passData <- c("nTrialsPerPerson", passData)    
+    passData <- c("nTrialsPerPerson" = EZ_stats$nTrialsPerPerson, passData)    
   }
   
   # For models that include predictors (i.e., metaregression, t-test),
   # we also need to pass the predictor vector X
   if(!(is.na(modelType)|modelType=="hierarchical")){
           # Add the predictor variable X to the list of data to pass
-          passData <- c(passData, "X")
+          passData <- c(passData, "X" = EZ_stats$X)
   }
   
   # Return the list of data to pass to JAGS
