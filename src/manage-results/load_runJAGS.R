40616af4650afc55c7803b32d8449c79694c36bc
Adrifelcha <adrifelcha@gmail.com>
2025-06-27 17:52:33 -0700

    Remove this.seed as an input, and instead have it be retrieved from the runJAGS function (which may result from bad rhats)

--- src/manage-results/load_runJAGS.R
+++ src/manage-results/load_runJAGS.R
@@ -1,5 +1,5 @@
-jags_localResults <- function(runJags_i, EZ_variation, data_variation, true_parameter_set, this.seed){
-    localResults <- list(seed = this.seed,           # Seed used for this cell (could change if R-hats were bad)
+jags_localResults <- function(runJags_i, EZ_variation, data_variation, true_parameter_set){
+    localResults <- list(seed = runJags_i$this.seed,           # Seed used for this cell (could change if R-hats were bad)
                          p = runJags_i$jags_data$nParticipants,                      # Number of participants
                          t = runJags_i$jags_data$nTrialsPerCondition,                # Number of trials
                          ez_type = EZ_variation,                                           # Design type                         
@@ -9,16 +9,16 @@ jags_localResults <- function(runJags_i, EZ_variation, data_variation, true_para
                          mean.estimates = runJags_i$estimates,         # Posterior means
                          std.estimates = runJags_i$estimates,          # Posterior SDs
                          elapsed.time = runJags_i$clock,               # Computation time
-                         this.seed = runJags_i$this.seed,             # Seed used for this cell (could have changed if R-hats were bad)
                          nIter = runJags_i$nIter,                      # Final used number of iterations
                          nBurnin = runJags_i$nBurnin,                 # Final used number of burn-in iterations
                          nThin = runJags_i$nThin,                     # Final used number of thinning iterations
                          bad_rhat_count = runJags_i$bad_rhat_count    # Number of times R-hats were bad                         
                         )
+                        
     return(localResults)
 }
 
-load_JAGS_cellResults <- function(runJags, this.seed, parameter_set){
+load_JAGS_cellResults <- function(runJags, parameter_set){
     
     EZ_variations <- names(runJags)
     data_variations <- names(runJags$EZ)
@@ -32,8 +32,7 @@ load_JAGS_cellResults <- function(runJags, this.seed, parameter_set){
                 # Store results in named list element
                 output[[name]] <- jags_localResults(runJags_i = runJags[[EZ_var]][[data_var]], 
                                                   EZ_variation = EZ_var, 
-                                                  data_variation = data_var, 
-                                                  this.seed = this.seed,
+                                                  data_variation = data_var,
                                                   true_parameter_set = parameter_set)
             }            
         }else{
@@ -41,7 +40,6 @@ load_JAGS_cellResults <- function(runJags, this.seed, parameter_set){
             output[[name]] <- jags_localResults(runJags_i = runJags[[EZ_var]], 
                                               EZ_variation = EZ_var, 
                                               data_variation = runJags[[EZ_var]]$data_type,
-                                              this.seed = this.seed, 
                                               true_parameter_set = parameter_set)
         }     
     }
