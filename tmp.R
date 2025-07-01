b8e05a0c57c7447479f84e26379a60131227545a
Adrifelcha <adrifelcha@gmail.com>
2025-07-01 13:40:17 -0700

    Temporary file to explore

--- tmp.R
+++ tmp.R
@@ -1,27 +1,21 @@
+
+rm(list = ls())
 library(here)
+
+# Call the function within the src directory
+source(here("src", "load_allFunctions.R"))
+load_allCustomFunctions()
 directory <- here("demos", "simulation-study", "samples")
+object_name = "output"
 
-x <- load_seedOutput(directory)
+x <- load_seedOutput(directory, object_name)
 
-length(x)
 
-names(x)
-
-x$reps
-
-length(x$settings)
-
-rm(list = ls())
-library(here)
-load(here("demos", "simulation-study", "samples", "seed-1.RData"))
-ls()
-names(output)
+store_simStudyResults(output = x, saveTo = here("output", "simStudy_results"))
 
 
 rm(list = ls())
 library(here)
-load(here("demos", "simulation-study", "samples", "seed-2.RData"))
+load(here("demos", "simulation-study", "samples", "seed-20.RData"))
 ls()
 names(output)
-
-
