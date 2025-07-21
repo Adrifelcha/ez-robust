4cc8651bbd70fa15f3babe833296fe8d52faae56
Adriana Chavez <adrifelcha@gmail.com>
2025-07-20 21:42:37 -0700

    Update results folder and change order of plots

--- output/figures_AUC/makeFigures.R
+++ output/figures_AUC/makeFigures.R
@@ -4,11 +4,12 @@ source(here("src", "load_allFunctions.R"))
 load_allCustomFunctions()
 
 
-main_dir <- here("output", "simStudy_results")
+main_dir <- here("output", "RData_simStudy_results")
 output_dir <- here("output", "figures_AUC")
-
-# Plot with conditions on the x-axis
-plot_AUCgrid(main_dir = main_dir, output_dir = output_dir, plot_by = "condition")
 
 # Plot with beta levels on the x-axis
 plot_AUCgrid(main_dir = main_dir, output_dir = output_dir, plot_by = "beta") 
+
+
+# Plot with conditions on the x-axis
+plot_AUCgrid(main_dir = main_dir, output_dir = output_dir, plot_by = "condition")
