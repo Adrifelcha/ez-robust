e013ee7b818f1e1c5e56da533f4c360cd1d4b3a8
Adriana Chavez <adrifelcha@gmail.com>
2025-07-20 21:43:50 -0700

    Update folder to run on Turing server

--- output/RData_simStudy_results/storeResults.R
+++ output/RData_simStudy_results/storeResults.R
@@ -1,10 +1,14 @@
+rm(list = ls())
 library(here)
 source(here("src", "load_allFunctions.R"))
 load_allCustomFunctions()
 
 # Define directories
-seed_dir <- here("demos", "simulation-study", "samples-full_Jul8")
+seed_dir <- here("demos", "simulation-study", "samples")
 output_dir <- here("output", "RData_simStudy_results")
 #
 # Run the memory-safe processing function
 process_sim_data_by_cell(seed_dir = seed_dir, output_dir = output_dir)
+
+load(here("demos", "simulation-study", "samples", "seed-433.RData"))
+# load(here("demos", "simulation-study", "samples", "seed-433.RData"))
