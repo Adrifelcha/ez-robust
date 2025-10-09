94e29917d9f288aadb142885c8a107164190a4ce
Adrifelcha <adrifelcha@gmail.com>
2025-10-08 21:15:18 -0700

    Move legend to first panel

--- src/plotting/plot_AUCgrid.R
+++ src/plotting/plot_AUCgrid.R
@@ -87,7 +87,7 @@ plot_AUCgrid <- function(main_dir, output_dir, plot_by = "condition", y_range = 
           show_legend <- (t_level == max(t_levels)) && (p_level == max(p_levels))
           plot_cell_by_condition(auc_df, conditions, condition_labels, y_range, show_x_axis, show_y_axis, show_legend)
         } else {
-          show_legend <- (t_level == min(t_levels)) && (p_level == min(p_levels))
+          show_legend <- (t_level == max(t_levels)) && (p_level == min(p_levels))
           highlight_cell <- ifelse(as.numeric(p_level) * as.numeric(t_level) == 6400, 
                                    TRUE, FALSE)
           plot_cell_by_beta(auc_df, conditions, condition_labels, y_range, show_x_axis, show_y_axis, show_legend,
@@ -195,7 +195,7 @@ plot_cell_by_beta <- function(auc_df, conditions, condition_labels, y_range, sho
   }
 
   
-  condition_colors <- c("#d3540b", "#160f0fea", "#47D647", "#de77f3")
+  condition_colors <- c("#d3540b", "#160f0fea", "#47D647", "#E982FF")
   widths <- c(5,5,4,3)
   styles <- c(2,1,4,3)
   points <- c(19,17,15,18)
