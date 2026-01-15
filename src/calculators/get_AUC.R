c0b267ec2f6e8c2399ef12fbb0a536b7091d92e2
Adriana Chavez <adrifelcha@gmail.com>
2026-01-14 16:29:14 -0800

    Rename function: from compute_AUC() to get_AUC(). Add comments as well.

--- src/calculators/get_AUC.R
+++ src/calculators/get_AUC.R
@@ -1,9 +1,12 @@
 # Custom Function to compute the Area Under the Curve (AUC)
-# from a vector of false positive rates (fpr) and a vector 
-# of true positive rates (tpr)
+# Inputs:
+# - fpr: a vector of false positive rates
+# - tpr: a vector of true positive rates
+# Output:
+# - AUC: the Area Under the Curve
 ###################################################################
-compute_AUC <- function(fpr, tpr){
 
+get_AUC <- function(fpr, tpr){
   # Sort by false positive rate
   sorted_fpr <- order(fpr)
   fpr <- fpr[sorted_fpr]
@@ -12,5 +15,5 @@ compute_AUC <- function(fpr, tpr){
   # Calculate AUC using the trapezoidal rule
   auc <- sum(diff(fpr) * (tpr[-1] + tpr[-length(tpr)])) / 2
 
-return(auc)
+  return(auc)
 }
