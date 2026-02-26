6ed46fe9cfbab31c01cb2e392bbbd228d2b75172
Adriana Chavez <adrifelcha@gmail.com>
2026-02-26 11:57:37 -0800

    Add: A simulation-cell simulation design where boundary and drift are on the high end

---
+++ demos/simStudy_cells/highDrift-highBound/runSimStudy.R
@@ -0,0 +1,60 @@
+##########################################################
+# LOAD FUNCTIONS/PACKAGES
+##########################################################
+######## Load required R packages for parallel processing
+# Set working directory to repo root (this script is in demos/simulation-study/)
+library(here)
+library(foreach)
+library(doParallel)
+
+# Call the function within the src directory
+source(here("src", "load_allFunctions.R"))
+load_allCustomFunctions()
+
+##########################################################
+# START OUTPUT DIRECTORIES IF NEEDED
+##########################################################
+# A directory to store the simulation results
+output_dir <- here("demos", "simStudy_cells", "highDrift-highBound", "samples")
+dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
+# A directory to store the simulation logs
+log_dir <- here("demos", "simStudy_cells", "highDrift-highBound", "logs")
+dir.create(log_dir, recursive = TRUE, showWarnings = FALSE)
+
+##########################################################
+# LOAD DEFAULT SIMULATION SETTINGS
+##########################################################
+source(here("demos", "simStudy_setup", "simulation_settings.R"))
+
+##########################################################
+# SET CUSTOM SIMULATION SETTINGS
+##########################################################
+settings$participant_levels <- 160
+settings$true_means$drift_mean <- c(2, 3)
+settings$true_means$bound_mean <- c(3.5, 4)
+
+################################################################
+# Run simulation study
+################################################################
+if(exists("runSim") && runSim){
+        cores       <-  detectCores()
+        my.cluster  <-  makeCluster(cores[1]-4)
+
+        registerDoParallel(cl = my.cluster)
+        # Then modify your foreach call to use this combine function
+        resultado <- foreach(seed = 1:1000, 
+                        .errorhandling = "pass",
+                        .combine = combine_results
+                        ) %dopar% {
+                                Z <- simStudy_runFullSeed(seed = seed,
+                                                        settings = settings,
+                                                        forceRun = forceRun,
+                                                        include_EZ_Robust = settings$include_EZ_Robust,
+                                                        redo_if_bad_rhat = TRUE,
+                                                        rhat_cutoff = 1.05,
+                                                        prevent_zero_accuracy = FALSE,
+                                                        Show = TRUE)
+                        }
+        stopCluster(cl = my.cluster)
+}
+
