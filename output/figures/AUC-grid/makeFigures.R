daf5149ff7b48d03111cb381bf368635a7f3c016
Adriana Chavez <adrifelcha@gmail.com>
2026-03-06 17:04:54 -0800

    Add: Calling out plotting function for cell specific results

--- output/figures/AUC-grid/makeFigures.R
+++ output/figures/AUC-grid/makeFigures.R
@@ -6,23 +6,47 @@ source(here("src", "load_allFunctions.R"))
 
 load_allCustomFunctions()
 
+########################################################################################
+# F U L L     S I M U L A T I O N     S T U D Y  #######################################
+########################################################################################
 # Establish output directory
-output_dir <- here("output", "figures", "AUC-grid")
+output_dir <- here("output", "figures", "AUC-grid", "full-simulation")
 
-###########################################
-# Plot AUC grid for full simulation study
-###########################################
-label <- "full"
-main_dir <- here("output", "RData", "full-simulation")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Wide range of parameter values
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+label <- "wideRange"
+main_dir <- here("output", "RData", "full-simulation", "wide-parameters")
+
+plot_AUCgrid(main_dir = main_dir, output_dir = output_dir, plot_by = "beta", custom_title_label = label) 
+plot_AUCgrid(main_dir = main_dir, output_dir = output_dir, plot_by = "condition", custom_title_label = label)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Restricted range of parameter values
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+label <- "restrictedRange"
+main_dir <- here("output", "RData", "full-simulation", "restricted-parameters")
 
 plot_AUCgrid(main_dir = main_dir, output_dir = output_dir, plot_by = "beta", custom_title_label = label) 
 plot_AUCgrid(main_dir = main_dir, output_dir = output_dir, plot_by = "condition", custom_title_label = label)
 
-##################################################
-# Plot AUC grid for restricted simulation study
-##################################################
-label <- "restricted"
-main_dir <- here("output", "RData", "restricted-simulation")
+########################################################################################
+# C E L L     S I M U L A T I O N     S T U D Y  #######################################
+########################################################################################
+output_dir <- here("output", "figures", "AUC-grid", "cell-simulation")
+main_dir <- here("output", "RData", "cell-simulation")
+plot_AUCgrid(main_dir = main_dir, output_dir = output_dir, plot_by = "beta")
 
-plot_AUCgrid(main_dir = main_dir, output_dir = output_dir, plot_by = "beta", custom_title_label = label) 
-plot_AUCgrid(main_dir = main_dir, output_dir = output_dir, plot_by = "condition", custom_title_label = label)
+
+# Low drift and low bound
+cell_dir <- here("output", "RData", "cell-simulation", "lowDrift-lowBound")
+plot_AUCgrid(main_dir = cell_dir, output_dir = output_dir, plot_by = "beta", custom_title_label = label)
+plot_AUCgrid(main_dir = cell_dir, output_dir = output_dir, plot_by = "condition", custom_title_label = label)
+
+# High drift and high bound
+label <- "highDrift-highBound"
+cell_dir <- here("output", "RData", "cell-simulation", "highDrift-highBound")
+plot_AUCgrid(main_dir = cell_dir, output_dir = output_dir, plot_by = "beta", custom_title_label = label)
+plot_AUCgrid(main_dir = cell_dir, output_dir = output_dir, plot_by = "condition", custom_title_label = label)
+
+# High drift and low bound
