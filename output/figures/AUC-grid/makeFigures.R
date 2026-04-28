12bb0a119c9b7c93425f10be77633b3da2fb187a
Adriana Chavez <adrifelcha@gmail.com>
2026-04-28 15:54:32 -0700

    Update: A script to generate all figures used in the paper and more. These figures show the diagnostic accuracy of the simulation studies run (as measured by the AUC)

--- output/figures/AUC-grid/makeFigures.R
+++ output/figures/AUC-grid/makeFigures.R
@@ -8,32 +8,36 @@ load_allCustomFunctions()
 
 
 ########################################################################################
-# C E L L     S I M U L A T I O N     S T U D Y  #######################################
+# FOLLOW-UP   S I M U L A T I O N     S T U D Y  #######################################
 ########################################################################################
 output_dir <- here("output", "figures", "AUC-grid", "cell-simulation")
 main_dir <- here("output", "RData", "cell-simulation")
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# AUC nested by parameter (new function)
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Default settings: x_param = "drift", T = 40, beta = 0.2
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Side-by-side: AUC as a function of true parameter values (panels distinguish between low and high)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Mu_drift on the x-axis, beta = 0.2
+# T = 40 (default)
 plot_AUC_nested_by_param(main_dir = main_dir, output_dir = output_dir, x_param = "drift_mean")
+# T = 160
 plot_AUC_nested_by_param(main_dir = main_dir, output_dir = output_dir, x_param = "drift_mean", t_level_select = 160)
 
-# Alternate test: x-axis is boundary parameter
+# Mu_bound on the x-axis, beta = 0.2
+# T = 40 (default)
 plot_AUC_nested_by_param(main_dir = main_dir, output_dir = output_dir, x_param = "bound_mean")
+# T = 160
 plot_AUC_nested_by_param(main_dir = main_dir, output_dir = output_dir, x_param = "bound_mean", t_level_select = 160)
 
-# Plot nested simulation study results with T = 40 and T = 160
-plot_AUC_nested(main_dir = main_dir, output_dir = output_dir, plot_by = "beta", run_diagnose = FALSE)
-
-# Simplified 2 x 2 nested layout at one trial level T
-plot_AUC_nested_2x2(main_dir = main_dir, output_dir = output_dir, plot_by = "beta", run_diagnose = FALSE)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 2 x 2 AUC as a function of betaweight parameter for one T level, across high/low bound and low/high drift.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+plot_AUC_nested_2x2(main_dir = main_dir, output_dir = output_dir, run_diagnose = FALSE)
 
 
 ########################################################################################
 # F U L L     S I M U L A T I O N     S T U D Y  #######################################
-########################################################################################
+#########################################################################################
+# These simulations include 5 levels of trial size (T) and sample size (P)
 # Establish output directory
 output_dir <- here("output", "figures", "AUC-grid", "full-simulation")
 
@@ -43,8 +47,7 @@ output_dir <- here("output", "figures", "AUC-grid", "full-simulation")
 label <- "wideRange"
 main_dir <- here("output", "RData", "full-simulation", "wide-parameters")
 
-plot_AUCgrid_full(main_dir = main_dir, output_dir = output_dir, plot_by = "beta", custom_title_label = label) 
-plot_AUCgrid_full(main_dir = main_dir, output_dir = output_dir, plot_by = "condition", custom_title_label = label)
+plot_AUCgrid_full(main_dir = main_dir, output_dir = output_dir, custom_title_label = label) 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Restricted range of parameter values
@@ -52,7 +55,6 @@ plot_AUCgrid_full(main_dir = main_dir, output_dir = output_dir, plot_by = "condi
 label <- "restrictedRange"
 main_dir <- here("output", "RData", "full-simulation", "restricted-parameters")
 
-plot_AUCgrid_full(main_dir = main_dir, output_dir = output_dir, plot_by = "beta", custom_title_label = label) 
-plot_AUCgrid_full(main_dir = main_dir, output_dir = output_dir, plot_by = "condition", custom_title_label = label)
+plot_AUCgrid_full(main_dir = main_dir, output_dir = output_dir, custom_title_label = label) 
 
 
